93cf5b836fbed66cf2369f5fc6a26c12dfe9d8b4
muky-albaneey <mukyalbani1@gmail.com>
2024-05-22 11:19:13 +0100

    verified

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Verified from '@/components/Verified';
 import Verification from '../components/Verification'
+import Sms from '@/components/Sms';
 
 export default function Home() {
   return (
     <div>
-      <Verification />        
+      <Verification />
+      <Sms /> 
+      <Verified />        
     </div>
   );
 }
--- app/sms/page.tsx
+++
@@ -1,9 +0,0 @@
-import Sms from '../../components/Sms'
-
-export default function Home() {
-  return (
-    <div>    
-      <Sms />   
-    </div>
-  );
-}
